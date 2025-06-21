6ce7e0d65bdd47ce8755b859cfe65d9e7db9004b
cooh2222222222 <168754755+cooh2222222222@users.noreply.github.com>
2025-06-21 18:40:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ ${message.content}`;
     message.reply(`✅ 告知予約を受け付けたよ！！`);
   } catch (err) {
     console.error(err);
-    message.reply("⚠ データ抽出に失敗しました。もう一度試してください！");
+    message.reply("⚠ データ抽出に失敗しました。もう一度試してください!！");
   }
 });
 
